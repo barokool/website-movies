72d342b3f3c111f23c4796e3a29513fa0813a311
baro gia <knabao7a7@gmail.com>
2022-01-08 14:44:42 +0700

    adding hover img |

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -6,8 +6,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-import styles from "./style.module.scss";
-
 export default function Movies() {
   const settings = {
     dots: false,
